7ce04fad67fd11593c39cb126cbd4c864ab150cf
Vlad <mr.vladinho@mail.ru>
2024-10-26 18:56:20 +0300

    fourth commit

--- src/components/UI/buttons/HelpButton.vue
+++ src/components/UI/buttons/HelpButton.vue
@@ -1,7 +1,8 @@
 <template>
   <button 
     class="help-btn h-8 cursor-pointer bg-grey_color text-xs my-1.5 mr-1.5 px-2 py-2 lg:hover:text-white lg:hover:bg-deep_dark_blue lg:focus:text-white lg:focus:bg-deep_dark_blue"
-    @click="setInputVal()">
+    @click="setInputVal()"
+    @mouseenter="removeFocus()">
       <slot></slot>
   </button>
 </template>
@@ -18,7 +19,13 @@
         emit('setInputVal')
       }
 
-      return { setInputVal }
+      const removeFocus = () => {
+        document.querySelectorAll('.help-btn')?.forEach(item => {
+          item.blur()
+        }) 
+      }
+
+      return { setInputVal, removeFocus }
     }
   })
 </script>
